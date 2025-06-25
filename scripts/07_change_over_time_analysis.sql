9cdf57ba1a571ee10148623919eb6b49de5af8ae
Linh Nguyen <nklinh.620@gmail.com>
2025-06-25 16:10:31 +0700

    Create 07_change_over_time_analysis.sql

---
+++ scripts/07_change_over_time_analysis.sql
@@ -0,0 +1,59 @@
+/*
+===============================================================================
+Change Over Time Analysis
+===============================================================================
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+===============================================================================
+*/
+
+-- Analyse Sales Performance over Time
+-- >> Quick Date Functions
+SELECT
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date);
+
+-- >> Use DATETRUNC() to round the order date to the months (show the year and month in the same column)
+SELECT
+    DATETRUNC(month, order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date);
+
+-- >> Use FORMAT() to show specific date format
+SELECT
+    FORMAT(order_date, 'yyyy-MMM') AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM');
+
+/* NOTE:
+- If SQL running time is too slow, check execution plan to see where the issues originate. 
+- In case the issue lies in the sorting process, we should create nonclustered columnstored index 
+for the order_date to improve performance since it's frequently filtered and grouped in the queries.
+
+-->> Run the below code before the Change Over Time Analysis:
+CREATE NONCLUSTERED COLUMNSTORE INDEX IX_FactSales_OrderDate ON gold.fact_sales (order_date)
+UPDATE STATISTICS gold.fact_sales WITH FULLSCAN; -- Update statistics after creating index or if data has changed significantly
+*/
